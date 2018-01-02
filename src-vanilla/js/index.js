27fbb78077e71cb6e3bcee3d6fe85547e1400cfa
connorjohnlind <connorjohnlind@gmail.com>
2018-01-02 15:29:09 -0800

    Added Basic Vanilla JS Event Listeners

--- src-vanilla/js/index.js
+++ src-vanilla/js/index.js
@@ -1,13 +1,44 @@
-const taskInput = document.querySelector('input.task');
+// TO DO APP
+const App = {
 
-taskInput.addEventListener("click", function() {
-  console.log('this is ' + this);
-  this.focus();
-  this.setSelectionRange(0, this.value.length);
-});
+  init() {
+    this.cacheDom();
+    this.bindEvents();
+    this.render();
+  },
 
-taskInput.addEventListener("keypress", function() {
-  if (event.which === 13) {
-    console.log('yo');
-  }
-});
+  cacheDom() {
+    this.taskInput = document.querySelector('input.task');
+  },
+
+  bindEvents() {
+
+    this.taskInput.addEventListener("click", function() {
+      console.log('this is ' + this);
+      this.focus();
+    });
+
+    this.taskInput.addEventListener("keypress", function() {
+      if (event.which === 13) {
+        const div = document.createElement("div");
+        div.innerHTML = this.value;
+        this.parentNode.insertBefore(div, this.nextSibling);
+        this.value = "";
+      }
+    });
+  },
+
+  render() {
+
+  },
+
+  addTask() {
+
+  },
+
+  removeTask(){
+
+  },
+};
+
+App.init();
