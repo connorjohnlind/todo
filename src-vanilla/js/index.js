b8dfefd42ea30595a914e2cf349367a007e097fb
connorjohnlind <connorjohnlind@gmail.com>
2018-01-02 17:03:59 -0800

    Created Task Array

--- src-vanilla/js/index.js
+++ src-vanilla/js/index.js
@@ -1,44 +1,49 @@
 // TO DO APP
 const App = {
 
+  data: [],
+
   init() {
     this.cacheDom();
     this.bindEvents();
-    this.render();
-  },
+    this.render(); },
 
   cacheDom() {
-    this.taskInput = document.querySelector('input.task');
+    this.taskInput = document.querySelector('input.new-task');
+    this.taskList = document.querySelector('div.task-list');
   },
 
   bindEvents() {
 
-    this.taskInput.addEventListener("click", function() {
-      console.log('this is ' + this);
-      this.focus();
+    this.taskInput.addEventListener("focus", ()=>{
+      //this.taskInput.blur();
     });
 
-    this.taskInput.addEventListener("keypress", function() {
-      if (event.which === 13) {
-        const div = document.createElement("div");
-        div.innerHTML = this.value;
-        this.parentNode.insertBefore(div, this.nextSibling);
-        this.value = "";
+    this.taskInput.addEventListener("keypress", ()=>{
+      if (event.which == 13 || event.keyCode == 13){
+        this.addTask(this.taskInput.value);
+        this.taskInput.value = "";
       }
     });
   },
 
   render() {
-
+    this.taskList.innerHTML = "";
+    this.data.forEach((el)=>{
+      const div = document.createElement("div");
+      div.innerHTML = el.text;
+      this.taskList.append(div);
+    })
   },
 
-  addTask() {
-
+  addTask(value) {
+    this.data.push({text: value, completed: false});
+    this.render();
   },
 
   removeTask(){
-
   },
+
 };
 
 App.init();
