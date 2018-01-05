6f024e7e97d45570fc9800e09a318c37dc1d92ae
connorjohnlind <connorjohnlind@gmail.com>
2018-01-04 17:10:03 -0800

    Added Event Handlers for Menu, and Additional Styling

--- src-vanilla/js/app.js
+++ src-vanilla/js/app.js
@@ -3,28 +3,114 @@ import Builder from './builder.js';
 // TO DO APP
 const App = {
 
+  // source of truth - related to DOM tasks via text property
   data: [],
 
   init() {
     this.cacheDom();
     this.render();
-    this.bindEvents();
+    this.bindStatelessEvents();
   },
 
   cacheDom() {
     this.$taskInput = document.querySelector('input.task-input');
     this.$taskContainer = document.querySelector('div.task-container');
     this.$checkAll = document.querySelector('.check-all');
+    this.$menu = document.querySelector('.menu');
+    this.$count = document.querySelector('.count');
+    this.$filters = Array.from(document.querySelector('.filters').children) || [];
+    this.$filterAll = document.querySelector('#filter-all');
+    this.$filterActive = document.querySelector('#filter-active');
+    this.$filterCompleted = document.querySelector('#filter-completed');
+    this.$clear = document.querySelector('.clear');
     this.$allTasks = Array.from(document.querySelectorAll('.task')) || [];
   },
 
-  bindEvents() {
+  bindStatelessEvents() {
     this.$taskInput.addEventListener("keypress", ()=>{
-      if (event.which == 13 || event.keyCode == 13){
+      if ((event.which == 13 || event.keyCode == 13) && this.$taskInput.value !== ""){
         this.addTask(this.$taskInput.value);
         this.$taskInput.value = "";
-        this.$checkAll.className = "check-all";  // removes invisible class
       }
+    });
+
+    this.$checkAll.addEventListener("click", ()=> {
+      // mark all as completed if task statuses are mixed
+      let toggle = true;
+      // if all tasks have identical status, just swap them
+      if (identicalStatus(this.data).identical) {
+        toggle = !identicalStatus(this.data).value;
+      }
+      this.$allTasks.forEach((div)=>{
+        this.toggleCompleted(div.children[1].innerText, toggle);
+      });
+    });
+
+    this.$clear.addEventListener("click", ()=> {
+      let taskRemovals = [];
+      this.data.forEach((el)=>{
+        if (el.completed===true) {
+          taskRemovals.push(el.text);
+        }
+      });
+      this.removeTasks(taskRemovals);
+    });
+
+
+    // MAJOR ISSUE!!! Filters get messed up after new taks are added
+    // Need to incorporate the filters with the render, not make them stateless event listeners
+
+
+    this.$filterAll.addEventListener("click", ()=> {
+      this.$allTasks.forEach((div)=>{
+        div.className = "task flex row";
+      });
+      this.$filters.forEach(el=>el.className="");
+      this.$filterAll.className = "selected";
+    });
+
+    this.$filterActive.addEventListener("click", ()=> {
+      this.$allTasks.forEach((div)=>{
+        div.firstChild.checked === false
+         ? div.className = "task flex row"
+         : div.className = "task flex row hidden";
+      });
+      this.$filters.forEach(el=>el.className="");
+      this.$filterActive.className = "selected";
+    });
+
+    this.$filterCompleted.addEventListener("click", ()=> {
+      this.$allTasks.forEach((div)=>{
+        div.firstChild.checked === true
+         ? div.className = "task flex row"
+         : div.className = "task flex row hidden";
+      });
+      this.$filters.forEach(el=>el.className="");
+      this.$filterActive.className = "";
+    });
+  },
+
+  bindStatefulEvents() {
+    this.$allTasks.forEach((div)=>{
+      // div.lastChild === 'X' button
+      div.addEventListener("mouseover", ()=>{
+        div.lastChild.className = "remove"; // removes invisible class
+      });
+
+      div.addEventListener("mouseleave", ()=>{
+        div.lastChild.className = "remove invisible"; // removes invisible class
+      });
+
+      div.lastChild.addEventListener("click", ()=>{
+        // to do: remove event listeners to prevent memory leaks, must use named event handler function
+        //        perhaps need to extrapolate all event handlers to a module
+        // relate the DOM div to the data array and remove both
+        this.removeTasks(div.children[1].innerText.split());
+      });
+
+      div.firstChild.addEventListener("click", ()=>{
+        this.toggleCompleted(div.children[1].innerText);
+      })
     });
   },
 
@@ -33,11 +119,34 @@ const App = {
     this.render();
   },
 
-  removeTask(text){
-    this.data = this.data.filter(function(el) {
-      return el.text !== text;
-    });
+  filterTasks(completed) {
+
+  },
+
+  removeTasks(taskTextArray){
+    taskTextArray.forEach((taskText)=>{
+      this.data = this.data.filter((el)=>{
+        return el.text !== taskText;
+      });
+    })
     this.render();
+  },
+
+  // optional value param sets the value
+  toggleCompleted(taskText, value=null) {
+    this.data.forEach((el)=>{
+      if (el.text === taskText) {
+        el.completed = value || !el.completed;
+      }
+    })
+    this.render();
+  },
+
+  getTaskCount(){
+    let result = this.data.filter((el)=>{
+      return el.completed === false;
+    });
+    return result.length;
   },
 
   render() {
@@ -46,34 +155,46 @@ const App = {
 
     if (this.data.length > 0){
 
-      // overwrite the allTasks array
-      // and append each task to the DOM
+      /****** TASKS RENDER *******/
+
+      // overwrite the allTasks DOM node array and append each task to the DOM
       this.$allTasks = Builder.buildTasks(this.data);
-      this.$allTasks.forEach((div)=>{
-        this.$taskContainer.append(div);
-      });
+      this.$allTasks.forEach((div)=>{this.$taskContainer.append(div)});
 
       // bind events to each new task
-      this.$allTasks.forEach((div)=>{
-        div.addEventListener("mouseover", ()=>{
-          div.lastChild.className = "remove"; // removes invisible class
-        });
-        div.addEventListener("mouseleave", ()=>{
-          div.lastChild.className = "remove invisible"; // removes invisible class
-        });
-        div.lastChild.addEventListener("click", ()=>{
-          // relate the DOM div to the DATA array and remove it
-          this.removeTask(div.children[1].innerText);
-        });
-      });
+      this.bindStatefulEvents();
 
-      // update the filter footer
-      let div = Builder.buildFilters(this.data);
-      this.$taskContainer.append(div);
+      /****** INPUT RENDER *******/
+      // remove invisible class
+      this.$checkAll.className = "check-all";
 
-    };
+      /****** MENU RENDER *******/
 
+      // update active task count
+      this.$count.innerHTML = `${this.getTaskCount()} Tasks Left`;
+
+      // show the clear button
+      this.data.length > this.getTaskCount()
+        ? this.$clear.className = "clear"
+        : this.$clear.className = "clear invisible";
+
+      // show the menu
+      this.$menu.className = "menu flex row";
+
+    } else {
+      this.$checkAll.className = "check-all invisible"
+      this.$menu.className = "menu flex row hidden";
+    }
   }
 };
+
+function identicalStatus(array) {
+    for(var i = 0; i < array.length - 1; i++) {
+        if(array[i].completed !== array[i+1].completed) {
+            return {identical: false, value: null};
+        }
+    }
+    return {identical: true, value: array[0].completed};
+}
 
 export default App;
--- src-vanilla/js/builder.js
+++ src-vanilla/js/builder.js
@@ -20,34 +20,14 @@ const Builder = {
       button.className = "remove invisible";
       button.innerHTML = "x";
 
+      //state check
+      el.completed ? input.checked = true : input.checked = false;
+
       div.append(input, label, button);
       divArray.push(div);
     })
 
     return divArray;
-  },
-
-  buildFilters(data) {
-    const div = document.createElement("div"),
-          span = document.createElement("div"),
-          ul = document.createElement("ul"),
-          li1 = document.createElement("li"),
-          li2 = document.createElement("li"),
-          li3 = document.createElement("li"),
-          button = document.createElement("button");
-
-    div.className = "menu flex row";
-    span.innerHTML = "XYZ Tasks Left";
-    ul.className = "filters flex row";
-    li1.innerHTML = "All";
-    li2.innerHTML = "Active";
-    li3.innerHTML = "Completed";
-    button.innerHTML = "Clear Completed";
-
-    ul.append(li1, li2, li3);
-    div.append(span, ul, button);
-
-    return div;
   }
 }
 
